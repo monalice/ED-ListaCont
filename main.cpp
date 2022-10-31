42700289d3cb3b1a3266543a53698e9b9283f027
Alice Amorim <63621173+monalice@users.noreply.github.com>
2022-10-31 15:27:18 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Ponto.h"
+#include "ListaCont.h"
+
+using namespace std;
+
+int main()
+{
+    ListaCont l(10);
+
+    l.insereFinal(24);
+    l.insereFinal(3);
+    l.insereFinal(2);
+    l.insereFinal(-1);
+    l.insereFinal(99);
+    l.imprime();
+
+    int vet[] = {1,2,3};
+    l.insereValores(3, vet);
+
+    l.imprime();
+
+    return 0;
+}
